7baad020835b0d9dd1cf3f82bfe24e3c08e219f5
ybq <769062159@qq.com>
2024-08-26 00:50:42 +0800

    update

--- cow-webui-vite/vite.config.js
+++ cow-webui-vite/vite.config.js
@@ -13,11 +13,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://wx.aigcboundless.cn', // 确保这是你的后端服务器地址
+        target: process.env.VITE_BASE_API, // 确保这是你的后端服务器地址
         changeOrigin: true,
         secure: true, // 添加此项以确保代理到 HTTPS 地址
         timeout: 60000, // 设置超时时间为 60 秒
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
