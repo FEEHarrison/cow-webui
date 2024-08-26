818b863878b58108e4e3cd7295fc3dc3164bc44a
ybq <769062159@qq.com>
2024-08-27 02:02:26 +0800

    ipdate

--- stop.sh
+++ stop.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# 查找并杀死Python服务的进程
+# 获取所有 gunicorn 进程的 PID
+PIDS=$(ps aux | grep '[g]unicorn' | awk '{print $2}')
 
-# 定义要结束的进程关键字
-pkill -f "gunicorn --timeout 120 --log-level debug -w 4 -b 0.0.0.0:5002 app:app"
-
-# 检查是否成功终止进程
-if [ $? -eq 0 ]; then
-    echo "成功终止 gunicorn 进程"
+if [ -z "$PIDS" ]; then
+  echo "没有找到正在运行的 gunicorn 进程"
 else
-    echo "未能终止 gunicorn 进程"
+  echo "找到以下 gunicorn 进程："
+  echo "$PIDS"
+  
+  # 终止所有 gunicorn 进程
+  kill $PIDS
+  
+  if [ $? -eq 0 ]; then
+    echo "成功终止 gunicorn 进程"
+  else
+    echo "终止 gunicorn 进程时发生错误"
+  fi
 fi
